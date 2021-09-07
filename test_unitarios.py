7c527f29ace47fb6c8d3fd7384b92bc51a95fb78
clariB17 <82007547+clariB17@users.noreply.github.com>
2021-09-07 03:40:33 -0300

    Add files via upload

---
+++ test_unitarios.py
@@ -0,0 +1,10 @@
+from numerosaletras import *
+
+a = numero_a_letra(123)#CIENTO VEINTI TRES
+print(a)
+
+b = numero_a_letra(1234567891011) #UN BILLON DOSCIENTOS TREINTA Y CUATRO MIL MILLLONES QUINIENTOS SESENTA Y SIETE MILLONES OCHOCIENTOS NOVENTA Y UN MIL ONCE
+print(b)
+
+c = numero_a_letra(200000000) # DOSCIENTOS MILLONES
+print(c)
